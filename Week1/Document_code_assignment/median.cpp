f0be55c94035a85c820c946af403e1521bebd487
Rostyslav Skrypnyk <rostyslav.skrypnyk@gmail.com>
2020-04-05 21:50:53 +0200

    Added Doxygen configuration file and added doc strings to the assignemnt.

--- Week1/Document_code_assignment/median.cpp
+++ Week1/Document_code_assignment/median.cpp
@@ -1,3 +1,21 @@
+/*!
+  \mainpage median program
+
+  This is a program that computes median value from those provided by user at runtime using CLI.
+
+  \section install_sec Compilation
+
+  `g++ -std=c++14 median.cpp -o median`
+
+  \section Usage
+
+  Run the program and start entering numbers separated by whitespace (blanks, tabs, and newlines).
+  Enter any non-numeric symbol to finnish entering. Program will print the median value out.
+
+  \file
+  \brief Implementation of the median functionality.
+*/
+
 #include <algorithm>
 #include <iostream>
 #include <memory>
@@ -8,12 +26,23 @@ using Data = std::unique_ptr<std::vector<double>>;
 Data read_data(std::istream& istream);
 double median(const std::vector<double>* values);
 
+/*!
+  \brief Calls functions to read input data and compute median value.
+         Prints output to screen.
+   \return Zero upon successful completion.
+*/
 int main() {
     Data values = read_data(std::cin);
     std::cout << median(values.get());
     return 0;
 }
 
+/*!
+  \brief Reads numeric data from input stream until non-numeric value is entered.
+  \param istream Reference to input stream object, from which the entered by user
+                 symbol is read.
+  \return `std::vector` of entered values in double precision.
+*/
 Data read_data(std::istream& istream) {
     auto values = std::make_unique<std::vector<double>>();
     double value;
@@ -22,6 +51,11 @@ Data read_data(std::istream& istream) {
     return values;
 }
 
+/*!
+  \brief Implementation of algorithm to compute median value of a sequence of numbers.
+  \param values Pointer to constant `std::vector` that holds sequence of double precision data.
+  \return Median value.
+ */
 double median(const std::vector<double>* values) {
     std::vector<double> tmp;
     std::copy(values->cbegin(), values->cend(), std::back_inserter(tmp));
@@ -33,6 +67,11 @@ double median(const std::vector<double>* values) {
         return tmp[index];
 }
 
+/*!
+  \brief Prints xdata to output stream.
+  \param values Pointer to constant `std::vector` that holds sequence of double precision data.
+  \return Void.
+*/
 void print_data(const std::vector<double>* values) {
     for (auto it = values->cbegin(); it != values->cend(); ++it)
         std::cout << *it << " ";
